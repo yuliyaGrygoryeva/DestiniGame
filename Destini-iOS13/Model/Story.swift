510685753c713bada21e9c1e04112701e2e581b9
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-08-16 15:40:03 -0700

    Story structure created. Navigation is working correct

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+struct Story {
+    var title: String
+    var choice1: String
+    var choice2: String
+    let choice1Destination: Int
+    let choice2Destination: Int
+    
+}
